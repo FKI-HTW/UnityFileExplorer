dcb4f4cad72d963575a3bf88aa60ce770983b4fb
jKnepel <knepel.julian@tutanota.com>
2023-12-06 19:33:27 +0100

    chore: switch gameobject prefabs to buttons

--- Runtime/Scripts/ExplorerConfiguration.cs
+++ Runtime/Scripts/ExplorerConfiguration.cs
@@ -14,21 +14,21 @@ namespace CENTIS.UnityFileExplorer
         public UINode FilePrefab { get => _filePrefab; set => _filePrefab = value; }
         [SerializeField] private UINode _filePrefab;
 
-        public GameObject ArrowBackPrefab { get => _arrowBackPrefab; set => _arrowBackPrefab = value; }
-        [SerializeField] private GameObject _arrowBackPrefab;
+        public Button ArrowBackPrefab { get => _arrowBackPrefab; set => _arrowBackPrefab = value; }
+        [SerializeField] private Button _arrowBackPrefab;
 
-        public GameObject ArrowForwardPrefab { get => _arrowForwardPrefab; set => _arrowForwardPrefab = value; }
-        [SerializeField] private GameObject _arrowForwardPrefab;
+        public Button ArrowForwardPrefab { get => _arrowForwardPrefab; set => _arrowForwardPrefab = value; }
+        [SerializeField] private Button _arrowForwardPrefab;
 
-        public GameObject ExitButtonPrefab { get => _exitButtonPrefab; set => _exitButtonPrefab = value; }
-        [SerializeField] private GameObject _exitButtonPrefab;
+        public Button ExitButtonPrefab { get => _exitButtonPrefab; set => _exitButtonPrefab = value; }
+        [SerializeField] private Button _exitButtonPrefab;
 
         // TODO : add cancel/choose file prefab
-        public GameObject CancelButtonPrefab { get => _cancelButtonPrefab; set => _cancelButtonPrefab = value; }
-        [SerializeField] private GameObject _cancelButtonPrefab;
+        public Button CancelButtonPrefab { get => _cancelButtonPrefab; set => _cancelButtonPrefab = value; }
+        [SerializeField] private Button _cancelButtonPrefab;
 
-        public GameObject ChooseFileButtonPrefab { get => _chooseFileButtonPrefab; set => _chooseFileButtonPrefab = value; }
-        [SerializeField] private GameObject _chooseFileButtonPrefab;
+        public Button ChooseFileButtonPrefab { get => _chooseFileButtonPrefab; set => _chooseFileButtonPrefab = value; }
+        [SerializeField] private Button _chooseFileButtonPrefab;
 
 
 		// TODO : add side column prefabs
