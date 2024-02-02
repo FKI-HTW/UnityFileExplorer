d2a9a1610e4ed6759d8e823ea5cd8b6d60b2f9a1
jKnepel <knepel.julian@tutanota.com>
2024-02-02 19:53:55 +0100

    refactor: use config file for references

--- Runtime/Scripts/ExplorerManager.cs
+++ Runtime/Scripts/ExplorerManager.cs
@@ -1,6 +1,5 @@
 using CENTIS.UnityFileExplorer.Datastructure;
 using UnityEngine;
-using UnityEngine.UI;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -9,47 +8,14 @@ namespace CENTIS.UnityFileExplorer
 {
     public class ExplorerManager : MonoBehaviour
     {
-		#region ui references
-
-		public GameObject FileExplorerContainer { get => _fileExplorerContainer; set => _fileExplorerContainer = value; }
-		[SerializeField] private GameObject _fileExplorerContainer;
-
-		public Button ArrowBackButton { get => _arrowBackButton; set => _arrowBackButton = value; }
-		[SerializeField] private Button _arrowBackButton;
-
-		public Button ArrowForwardButton { get => _arrowForwardButton; set => _arrowForwardButton = value; }
-		[SerializeField] private Button _arrowForwardButton;
-
-		public GameObject PathContainer { get => _pathContainer; set => _pathContainer = value; }
-		[SerializeField] private GameObject _pathContainer;
-
-		public UIPathFolder PathFolderPrefab { get => _folderButtonPrefab; set => _folderButtonPrefab = value; }
-		[SerializeField] private UIPathFolder _folderButtonPrefab;
-
-		public Button ExitButton { get => _exitButton; set => _exitButton = value; }
-		[SerializeField] private Button _exitButton;
-
-		public GameObject NodeContainer { get => _nodeContainer; set => _nodeContainer = value; }
-		[SerializeField] private GameObject _nodeContainer;
-
-		public UINode FolderPrefab { get => _folderPrefab; set => _folderPrefab = value; }
-		[SerializeField] private UINode _folderPrefab;
-
-		public UINode FilePrefab { get => _filePrefab; set => _filePrefab = value; }
-		[SerializeField] private UINode _filePrefab;
-
-		public GameObject EmptyFolderPrefab { get => _emptyFolderPrefab; set => _emptyFolderPrefab = value; }
-		[SerializeField] private GameObject _emptyFolderPrefab;
-
-		public Button CancelButton { get => _cancelButton; set => _cancelButton = value; }
-		[SerializeField] private Button _cancelButton;
-
-		public Button ChooseFileButton { get => _chooseFileButton; set => _chooseFileButton = value; }
-		[SerializeField] private Button _chooseFileButton;
-
-		#endregion
-
 		#region fields
+
+		[SerializeField] private ExplorerConfiguration _config;
+		public ExplorerConfiguration ExplorerConfiguration 
+		{ 
+			get => _config; 
+			set => _config = value; 
+		}
 
 		private VirtualFolderNode	_root; // a virtual folder above the disks
 		private VirtualFolderNode	_currentFolder;
@@ -84,14 +50,14 @@ namespace CENTIS.UnityFileExplorer
 			}
 
 			_fileFoundCallback = onFilePathFound;
-			_currentFolder = _root = new VirtualFolderNode(this, new() { Name = "This PC" }, null);
+			_currentFolder = _root = new VirtualFolderNode(_config, new() { Name = "This PC" }, null);
 			_hashedNodes.Add(_root.ToString(), _root);
 
 			// create drive folders beneath virtual root
 			DriveInfo[] drives = DriveInfo.GetDrives();
 			foreach (DriveInfo drive in drives)
 			{
-				FolderNode diskNode = new(this, drive.GetNodeInformation(), _root);
+				FolderNode diskNode = new(_config, drive.GetNodeInformation(), _root);
 				diskNode.OnSelected += SelectNode;
 				diskNode.OnDeselected += DeselectNode;
 				diskNode.OnActivated += ActivateNode;
@@ -110,7 +76,7 @@ namespace CENTIS.UnityFileExplorer
 			string startFolderPath = Environment.GetFolderPath((Environment.SpecialFolder)startFolder);
 			DirectoryInfo startDir = new(startFolderPath);
 			VirtualFolderNode startParent = FindParentRecursive(startDir);
-			FolderNode startNode = new(this, startDir.GetNodeInformation(), startParent);
+			FolderNode startNode = new(_config, startDir.GetNodeInformation(), startParent);
 			startNode.OnSelected += SelectNode;
 			startNode.OnDeselected += DeselectNode;
 			startNode.OnActivated += ActivateNode;
@@ -123,7 +89,7 @@ namespace CENTIS.UnityFileExplorer
 		public virtual void CancelFindFile()
 		{
 			_fileFoundCallback = null;
-			_root.Unload();
+			_root?.Unload();
 			_selectedNode = _currentFolder = _root = null;
 			_hashedNodes.Clear();
 			_lastVisitedNodes.Clear();
@@ -141,36 +107,36 @@ namespace CENTIS.UnityFileExplorer
 
 		private void InitFileExplorer()
 		{
-			if (ArrowBackButton != null)
+			if (_config.ArrowBackButton != null)
 			{
-				ArrowBackButton.onClick.RemoveListener(GoBack);
-				ArrowBackButton.onClick.AddListener(GoBack);
-				ArrowBackButton.interactable = false;
+				_config.ArrowBackButton.onClick.RemoveListener(GoBack);
+				_config.ArrowBackButton.onClick.AddListener(GoBack);
+				_config.ArrowBackButton.interactable = false;
 			}
 
-			if (ArrowForwardButton != null)
+			if (_config.ArrowForwardButton != null)
 			{
-				ArrowForwardButton.onClick.RemoveListener(GoForward);
-				ArrowForwardButton.onClick.AddListener(GoForward);
-				ArrowForwardButton.interactable = false;
+				_config.ArrowForwardButton.onClick.RemoveListener(GoForward);
+				_config.ArrowForwardButton.onClick.AddListener(GoForward);
+				_config.ArrowForwardButton.interactable = false;
 			}
 
-			if (ExitButton != null)
+			if (_config.ExitButton != null)
 			{
-				ExitButton.onClick.RemoveListener(CancelFindFile);
-				ExitButton.onClick.AddListener(CancelFindFile);
+				_config.ExitButton.onClick.RemoveListener(CancelFindFile);
+				_config.ExitButton.onClick.AddListener(CancelFindFile);
 			}
 
-			if (CancelButton != null)
+			if (_config.CancelButton != null)
 			{
-				CancelButton.onClick.RemoveListener(CancelFindFile);
-				CancelButton.onClick.AddListener(CancelFindFile);
+				_config.CancelButton.onClick.RemoveListener(CancelFindFile);
+				_config.CancelButton.onClick.AddListener(CancelFindFile);
 			}
 
-			if (ChooseFileButton != null)
+			if (_config.ConfirmButton != null)
 			{
-				ChooseFileButton.onClick.RemoveListener(ChooseSelectedNode);
-				ChooseFileButton.onClick.AddListener(ChooseSelectedNode);
+				_config.ConfirmButton.onClick.RemoveListener(ChooseSelectedNode);
+				_config.ConfirmButton.onClick.AddListener(ChooseSelectedNode);
 			}
 		}
 
@@ -225,7 +191,7 @@ namespace CENTIS.UnityFileExplorer
 		// TODO : optimize this
 		private void UpdatePath()
 		{
-			if (PathContainer == null) return;
+			if (_config.PathContainer == null) return;
 
 			foreach (PathNode node in _pathNodes)
 				GameObject.Destroy(node.UIInstance.gameObject);
@@ -242,7 +208,7 @@ namespace CENTIS.UnityFileExplorer
 			for (int i = _pathNodes.Count - 1; i >= 0; i--)
 			{
 				PathNode node = _pathNodes[i];
-				UIPathFolder uiInstance = Instantiate(PathFolderPrefab, PathContainer.transform);
+				UIPathFolder uiInstance = Instantiate(_config.PathFolderPrefab, _config.PathContainer.transform);
 				uiInstance.Initialize(node.Name);
 				uiInstance.OnActivated += () => ActivateNode(node.FolderNode);
 				node.UIInstance = uiInstance;
@@ -257,10 +223,10 @@ namespace CENTIS.UnityFileExplorer
 			_lastVisitedNodes.RemoveAt(_lastVisitedNodes.Count - 1);
 			_lastReturnedFromNodes.Add(_currentFolder);
 
-			if (ArrowForwardButton != null)
-				ArrowForwardButton.interactable = true;
-			if (ArrowBackButton != null)
-				ArrowBackButton.interactable = _lastVisitedNodes.Count != 0;
+			if (_config.ArrowForwardButton != null)
+				_config.ArrowForwardButton.interactable = true;
+			if (_config.ArrowBackButton != null)
+				_config.ArrowBackButton.interactable = _lastVisitedNodes.Count != 0;
 
 			_currentFolder.NavigateFrom();
 			_currentFolder = targetNode;
@@ -277,10 +243,10 @@ namespace CENTIS.UnityFileExplorer
 			_lastReturnedFromNodes.RemoveAt(_lastReturnedFromNodes.Count - 1);
 			_lastVisitedNodes.Add(_currentFolder);
 
-			if (ArrowBackButton != null)
-				ArrowBackButton.interactable = true;
-			if (ArrowForwardButton != null)
-				ArrowForwardButton.interactable = _lastReturnedFromNodes.Count != 0;
+			if (_config.ArrowBackButton != null)
+				_config.ArrowBackButton.interactable = true;
+			if (_config.ArrowForwardButton != null)
+				_config.ArrowForwardButton.interactable = _lastReturnedFromNodes.Count != 0;
 
 			_currentFolder.NavigateFrom();
 			_currentFolder = targetNode;
@@ -298,7 +264,7 @@ namespace CENTIS.UnityFileExplorer
 					AddFiles(node);
 					if (!node.IsFolderLoaded || node.Children.Count == 0)
 					{
-						EmptyNode emptyNode = new(this, node);
+						EmptyNode emptyNode = new(_config, node);
 						node.AddChild(emptyNode);
 					}
 				}
@@ -309,10 +275,10 @@ namespace CENTIS.UnityFileExplorer
 
 			_lastVisitedNodes.Add(_currentFolder);
 			_lastReturnedFromNodes.Clear();
-			if (ArrowBackButton != null)
-				ArrowBackButton.interactable = true;
-			if (ArrowForwardButton != null)
-				ArrowForwardButton.interactable = false;
+			if (_config.ArrowBackButton != null)
+				_config.ArrowBackButton.interactable = true;
+			if (_config.ArrowForwardButton != null)
+				_config.ArrowForwardButton.interactable = false;
 
 			_currentFolder.NavigateFrom();
 			_currentFolder = node;
@@ -327,7 +293,7 @@ namespace CENTIS.UnityFileExplorer
 			IEnumerable<DirectoryInfo> containedDir = new DirectoryInfo(folderPath).GetDirectories();
 			foreach (DirectoryInfo dir in containedDir)
 			{
-				FolderNode folderNode = new(this, dir.GetNodeInformation(), folder);
+				FolderNode folderNode = new(_config, dir.GetNodeInformation(), folder);
 				folderNode.OnSelected += SelectNode;
 				folderNode.OnDeselected += DeselectNode;
 				folderNode.OnActivated += ActivateNode;
@@ -344,7 +310,7 @@ namespace CENTIS.UnityFileExplorer
 			{
 				if (_certainFilesOnly && !file.Name.EndsWith(_fileExtension)) continue;
 
-				FileNode fileNode = new(this, file.GetNodeInformation(), folder);
+				FileNode fileNode = new(_config, file.GetNodeInformation(), folder);
 				fileNode.OnSelected += SelectNode;
 				fileNode.OnDeselected += DeselectNode;
 				fileNode.OnActivated += ActivateNode;
@@ -359,7 +325,7 @@ namespace CENTIS.UnityFileExplorer
 				return (FolderNode)parent;
 
 			FolderNode nextParent = FindParentRecursive(userDir.Parent);
-			FolderNode newNode = new(this, userDir.Parent.GetNodeInformation(), nextParent);
+			FolderNode newNode = new(_config, userDir.Parent.GetNodeInformation(), nextParent);
 			newNode.OnSelected += SelectNode;
 			newNode.OnDeselected += DeselectNode;
 			newNode.OnActivated += ActivateNode;
